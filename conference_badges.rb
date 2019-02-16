99ffb57e37b67fe07429e9f6993e12339772af0f
Kimberly Rouse <ksrouse94@gmail.com>
2019-02-16 21:36:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  
+  badge_message = []
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+  end  
+  return badge_message
 end  
